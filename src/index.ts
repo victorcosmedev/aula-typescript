82fa31f6d8ddd62a57817fa87cc8b4095e6003e1
Victor Cosme <120526474+victorcosmedev@users.noreply.github.com>
2025-02-19 20:44:38 -0300

    Exercício 3 - Classe e Instância

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-// let mensagem: string = "Olá, Typescript!";
-// console.log(mensagem);
+let mensagem: string = "Olá, Typescript!";
+console.log(mensagem);
 
-// let valores: number[] = [10, 20, 30];
-// let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
-// console.log("Soma dos valores:", somaValores);
+let valores: number[] = [10, 20, 30];
+let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores:", somaValores);
 
 function exibeMensagemPersonalizada(nome: string): string {
     return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
 }
 
 console.log(exibeMensagemPersonalizada("Victor"));
+
+class Carro {
+    constructor(public marca: string, public modelo: string, public ano: number) {}
+    
+    exibirDetalhes(): string {
+        return `${this.marca} ${this.modelo} - ${this.ano}`;
+    }
+}
+const carro = new Carro("Honda", "Civic", 1995);
+console.log(carro.exibirDetalhes());
