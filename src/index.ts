b6e6b89d5ed0a777698c04cebb6260380d84d823
Victor Cosme <120526474+victorcosmedev@users.noreply.github.com>
2025-02-19 20:33:45 -0300

    Exercício 1 - Tipagem e Arrays

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-let mensagem: string = "Olá, Typescript!";
-console.log(mensagem);
+// let mensagem: string = "Olá, Typescript!";
+// console.log(mensagem);
+
+let valores: number[] = [10, 20, 30];
+let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores:", somaValores);
