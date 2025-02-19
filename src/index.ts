bb182282f805d9de05c304a3e487f69cf6081482
Victor Cosme <120526474+victorcosmedev@users.noreply.github.com>
2025-02-19 20:40:26 -0300

    Exercício 2 - Função Tipada

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 // let mensagem: string = "Olá, Typescript!";
 // console.log(mensagem);
 
-let valores: number[] = [10, 20, 30];
-let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
-console.log("Soma dos valores:", somaValores);
+// let valores: number[] = [10, 20, 30];
+// let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+// console.log("Soma dos valores:", somaValores);
+
+function exibeMensagemPersonalizada(nome: string): string {
+    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+}
+
+console.log(exibeMensagemPersonalizada("Victor"));
